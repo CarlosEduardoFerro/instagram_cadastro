3aa4c2b457c4846ff757634149c340e8e39b7605
CarlosEduardoFerro <carlos.ferro@aluno.senai.br>
2025-02-07 12:35:31 -0300

    adicionado

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-const emailInput = document.getElementById('email');
-const senhaInput = document.getElementById('senha');
-const nomeInput = document.getElementById('nome');
-const usuarioInput = document.getElementById('usuario');
-const cadastroButton = document.getElementById('cadastro');
+document.getElementById("cadastro").addEventListener("click", function(){
 
-cadastroButton.addEventListener('click', (event) => {
-    event.preventDefault(); 
+    //capturei os elementos html
+    const email = document.querySelector("#email")
+    const senha = document.querySelector("#senha")
+    const nome = document.querySelector("#nome")
+    const usuario = document.querySelector("#usuario")
 
-    
-    if (
-        !emailInput.value.trim() ||
-        !senhaInput.value.trim() ||
-        !nomeInput.value.trim() ||
-        !usuarioInput.value.trim()
-    ) {
-        alert('Por favor, preencha todos os campos antes de prosseguir.');
-        return;
+    //extrair valor digitado pelo usuario no campo input
+    const emailValue = email.value;
+    const senhaValue = senha.value;
+    const nomeValue = nome.value;
+    const usuarioValue = usuario.value;
+
+    if(emailValue === ""){
+        email.classList.add("erro")
+    }else{
+        email.classList.add("ok")
     }
 
-    
-    window.location.href = 'boasvindas.html';
-});
+    if(senhaValue === ""){
+        senha.classList.add("erro")
+    }else{
+        senha.classList.add("ok")
+    }
+
+    if(nomeValue === ""){
+        nome.classList.add("erro")
+    }else{
+        nome.classList.add("ok")
+    }
+
+    if(usuarioValue === ""){
+        usuario.classList.add("erro")
+    }else{
+        usuario.classList.add("ok")
+    }
+})
